8ddeda20c51808c1e1c2a432351c682187a15773
Kaimandrilo <cporrassandoval@gmail.com>
2025-05-26 23:30:09 -0500

    ELEMENTOS FALTANTES EN MOSTRAR JUEGO

--- mostrarJuego.php
+++ mostrarJuego.php
@@ -6,10 +6,39 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $dsn = "mysql:host=$host;dbname=$base_de_datos;charset=utf8mb4";
     $miPDO = new PDO ($dsn,$usuario,$clave);
     $miPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     $miConsulta = $miPDO->prepare('SELECT * FROM videojuegos WHERE id = :id');
     $miConsulta->bindParam(':id', $id, PDO::PARAM_INT);
     $miConsulta->execute();
     $videojuego = $miConsulta->fetch(PDO::FETCH_ASSOC);
+
+    $resenas = $miPDO->prepare('SELECT resenas.*, usuarios.nombre_completo FROM resenas INNER JOIN usuarios ON usuarios.id = resenas.usuario_id WHERE videojuego_id = :id');
+    $resenas->bindParam(':id', $id, PDO::PARAM_INT);
+    $resenas->execute();
+
+    $stmt = $miPDO->prepare('
+    SELECT generos.nombre 
+    FROM generos 
+    INNER JOIN videojuego_genero 
+        ON videojuego_genero.genero_id = generos.id 
+    WHERE videojuego_genero.videojuego_id = :id
+    ');
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $generos = $stmt->fetchAll(PDO::FETCH_ASSOC);    
+
+    $stmt = $miPDO->prepare('
+    SELECT plataformas.nombre 
+    FROM plataformas 
+    INNER JOIN videojuego_plataforma 
+        ON videojuego_plataforma.plataforma_id = plataformas.id 
+    WHERE videojuego_plataforma.videojuego_id = :id
+    ');
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $plataformas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
 
@@ -54,21 +83,41 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
                 <h2><?php echo $videojuego['titulo'] ?></h2>
                 <p><strong>Desarrollador:</strong><?php echo $videojuego['desarrolladora'] ?></p>
                 <p><strong>Año de lanzamiento:</strong><?php echo $videojuego['año'] ?></p>
-                <p><strong>Género:</strong></p>
+                <p><strong>Género: 
+                   <?php 
+    if ($generos) {
+        $nombres = array_column($generos, 'nombre');
+        echo implode(', ', $nombres) . '.';
+    } else {
+        echo 'Sin género disponible.';
+    }
+?>
                 <p><strong>Modo de juego:</strong></p>
             </div>
         </div>
         <div class="game-extra-info">
             <h2>INFORMACIÓN VIDEOJUEGO</h2>
             <p><strong>De qué trata:</strong> <?php echo $videojuego['descripcion'] ?></p>
-            <p><strong>Disponible en:</strong> PC, PS3, PS4, PS5, Xbox 360, Xbox One, Xbox Series X/S.</p>
+            <p><strong>Disponible en:</strong>   
+                <?php 
+                    if ($plataformas) {
+                    $nombres = array_column($plataformas, 'nombre');
+                    echo implode(', ', $nombres) . '.';
+                    } else {
+                    echo 'Sin plataforma disponible.';
+                    }
+                ?></p>
         </div>
         <div class="container">
             <h2>Reseñas</h2>
-            <p><strong>Usuario 1:</strong> "La historia es genial y el modo online es adictivo."</p>
-            <p><strong>Rating:</strong> <span class="review-rating">★★★★★</span></p>
-            <p><strong>Usuario 2:</strong> "Gráficos realistas y muchas cosas por hacer. Lo juego desde hace años."</p>
-            <p><strong>Rating:</strong> <span class="review-rating">★★★★★</span></p>
+                    <?php foreach ( $resenas as $resenias):?>
+                    <p><strong><?php echo $resenias['nombre_completo']?>:</strong> <?php echo $resenias['comentario']?></p>
+                    <p><strong>calificación:</strong> <span class="review-rating">
+                        <?php for ($i = 0; $i < $resenias['calificacion']; $i++) {
+                        echo '⭐';
+                        }?>
+                    </span></p>
+                <?php endforeach ?>
         </div>
 
         <div id="formulario-reseña" style="display: none;">
